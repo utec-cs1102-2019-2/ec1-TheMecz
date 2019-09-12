ba7983afc7255c829f01f945a110cbe99882a552
TheMecz <maxponcesan@gmail.com>
2019-09-12 12:26:33 -0500

    Commit message

---
+++ session4b/cmake-build-debug/CMakeFiles/session4b1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session4b1.dir/ejercicio3.cpp.obj"
+  "session4b1.pdb"
+  "session4b1.exe"
+  "session4b1.exe.manifest"
+  "libsession4b1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session4b1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
